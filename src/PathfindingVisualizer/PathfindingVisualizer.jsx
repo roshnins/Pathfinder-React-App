597e58260fd38184dbc9dd32d444157b1a89d78b
roshnins <roshnins@hotmail.com>
2022-10-02 17:21:24 +0800

    Adding Start and End Nodes

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -16,7 +16,13 @@ export default class PathfindingVisualizer extends Component {
     for (let row = 0; row < 15; row++) {
       const currentRow = [];
       for (let col = 0; col < 50; col++) {
-        currentRow.push([]);
+        const currentNode = {
+          col,
+          row,
+          isStart: row === 10 && col === 5,
+          isFinish: row === 10 && col === 45,
+        };
+        currentRow.push(currentNode);
       }
       nodes.push(currentRow);
     }
@@ -31,10 +37,18 @@ export default class PathfindingVisualizer extends Component {
       <div className='grid'>
         {nodes.map((row, rowIdx) => {
           return (
-            <div>
-              {row.map((node, nodeIdx) => (
-                <Node></Node>
-              ))}
+            <div key={rowIdx}>
+              {row.map((node, nodeIdx) => {
+                const { isStart, isFinish } = node;
+                return (
+                  <Node
+                    key={nodeIdx}
+                    isStart={isStart}
+                    isFinish={isFinish}
+                    test={"foo"}
+                  ></Node>
+                );
+              })}
             </div>
           );
         })}
